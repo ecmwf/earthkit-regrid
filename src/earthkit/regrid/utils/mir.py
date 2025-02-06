d4da91cefc97c60ed88c70e0fb1d82b17aa75a21
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2025-02-06 22:53:37 +0000

    feature/gridspec: mir-python integration

--- src/earthkit/regrid/utils/mir.py
+++ src/earthkit/regrid/utils/mir.py
@@ -1,4 +1,4 @@
-# #!/usr/bin/env python
+#!/usr/bin/env python
 
 # (C) Copyright 2025- ECMWF.
 #
@@ -9,7 +9,6 @@
 # nor does it submit to any jurisdiction.
 #
 
-import subprocess
 from pathlib import Path
 from typing import Dict
 from typing import List
@@ -89,60 +88,6 @@ def mir_write_latlon_to_griddef(path, lats, lons):
             s.write_double(lon)
 
 
-def mir_make_matrix(in_lat, in_lon, out_lat, out_lon, output=None, mir=None, **kwargs):
-    import os
-    import shutil
-    from tempfile import TemporaryDirectory
-
-    mir = mir or os.getenv("MIR_COMMAND", "mir")
-
-    ext = Path(output).suffix if output is not None else None
-    if output is not None and ext not in (".mat", ".npz"):
-        raise ValueError("mir_make_matrix: output must have extension .mat or .npz")
-
-    with TemporaryDirectory() as tmpdir:
-        cwd = Path(tmpdir)
-        env = os.environ.copy()
-        env["MIR_DEBUG"] = "1"
-        env["MIR_CACHE_PATH"] = tmpdir
-
-        mir_write_latlon_to_griddef(cwd / "in.griddef", in_lat, in_lon)
-        mir_write_latlon_to_griddef(cwd / "out.griddef", out_lat, out_lon)
-
-        cmd = [
-            mir,
-            os.devnull,
-            os.devnull,
-            (
-                "--input={artificialInput:constant,constant:0.,gridded:True,"
-                "gridType:unstructured_grid,griddef:in.griddef}"
-            ),
-            "--griddef=out.griddef",
-            *[f"--{k}={v}" for k, v in kwargs.items()],
-        ]
-
-        try:
-            subprocess.run(cmd, check=True, cwd=cwd, env=env)
-
-            matrices = list(cwd.rglob("*.mat"))
-            if not matrices:
-                raise FileNotFoundError("mir_make_matrix: no matrix file found in output directory.")
-
-            if len(matrices) > 1:
-                raise RuntimeError("mir_make_matrix: multiple matrix files found in output directory.")
-
-            if output is None:
-                return mir_cached_matrix_to_array(matrices[0])
-
-            if ext == ".npz":
-                mir_cached_matrix_to_file(matrices[0], output)
-            else:
-                shutil.move(matrices[0], output)
-
-        except Exception as e:
-            raise RuntimeError(f"mir_make_matrix: error: {e}.") from e
-
-
 def _yaml_from_dict(d):
     import yaml
 
@@ -166,7 +111,7 @@ def _griddef_from_latlon(lat, lon, dir=None):
     return str(name)
 
 
-def mir_make_another_matrix(
+def mir_make_matrix(
     in_grid: Optional[Dict] = None,
     in_lat: Optional[List] = None,
     in_lon: Optional[List] = None,

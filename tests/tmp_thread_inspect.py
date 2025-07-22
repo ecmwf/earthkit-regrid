00f14993f2d457ef983769623f076b61bfc7457d
Sandor Kertesz <Sandor.Kertesz@ecmwf.int>
2025-07-22 18:39:09 +0100

    Modify tests

--- tests/tmp_thread_inspect.py
+++ tests/tmp_thread_inspect.py
@@ -25,21 +25,32 @@ def __main__():
     import mir
     import numpy as np
 
+    print("1", flush=True)
     values = np.random.rand(192)
 
+    print("2", flush=True)
     in_grid = {"grid": "H4", "order": "nested"}
     out_grid = {"grid": [10, 10]}
 
+    print("3", flush=True)
     input = mir.ArrayInput(values, in_grid)
+
+    print("4", flush=True)
     out = mir.ArrayOutput()
 
+    print("5", flush=True)
     job = mir.Job()
     job.set("grid", out_grid)
     job.set("interpolation", "linear")
+
+    print("6", flush=True)
     job.execute(input, out)
 
+    print("7", flush=True)
     assert len(out.values())
     assert out.spec
+
+    print("8", flush=True)
 
 
 if __name__ == "__main__":

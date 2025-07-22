b282db7ccf91fd57c8a41ca406614cdf93b778ab
Sandor Kertesz <Sandor.Kertesz@ecmwf.int>
2025-07-22 18:49:23 +0100

    Modify tests

--- tests/tmp_thread_inspect.py
+++ tests/tmp_thread_inspect.py
@@ -1,35 +1,14 @@
-# from earthkit.regrid import regrid
-
-# grids = [
-#     ({"grid": "H4", "order": "nested"}, {"grid": [10, 10]}),
-#     ({"grid": "h4", "order": "nested"}, {"grid": [10, 10]}),
-# ]
-
-
-# def run_example(in_grid, out_grid):
-#     import numpy as np
-
-#     values = np.random.rand(192)
-
-#     # TODO: make this code work
-#     values_res, _ = regrid(values, in_grid, out_grid, interpolation="linear")
-
-
-# def __main__():
-#     for gs_in, gs_out in grids:
-#         print(f"Regridding from {gs_in} to {gs_out}")
-#         run_example(gs_in, gs_out)
-
-
 def __main__():
     import mir
     import numpy as np
 
     print("1", flush=True)
     values = np.random.rand(192)
+    values = np.random.rand(37, 72)
 
     print("2", flush=True)
     in_grid = {"grid": "H4", "order": "nested"}
+    in_grid = {"grid": [5, 5]}
     out_grid = {"grid": [10, 10]}
 
     print("3", flush=True)
